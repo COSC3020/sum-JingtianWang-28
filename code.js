d95a019a9b690512632fe0dd4601810491adf246
JingtianWang-28 <144759925+JingtianWang-28@users.noreply.github.com>
2024-12-10 14:59:25 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,10 @@
 function sum(a) {
-    var sum = 0; 
-    for (var i = 0; i < a.length; i++) {
-        sum += a[i]; 
+    if (!Array.isArray(a)) return 0;
+    let sum = 0; 
+    for (let i = 0; i < a.length; i++) {
+        if (typeof a[i] !== "number") return 0;
+        sum += a[i];
     }
     return sum;
 }
-console.log(sum([1,2,3]));
+console.log(sum([1, 2, 3])); 
